64aabffefff461540097dc843f2c69d4b8d50d46
Arnaud Bergeron <abergeron@gmail.com>
2022-09-20 14:40:53 -0400

    Make the side garbage of fuzzing more manageable

--- libsquash/src/bin/squashfuzz.rs
+++ libsquash/src/bin/squashfuzz.rs
@@ -3,8 +3,12 @@ extern crate afl;
 
 use libsquash::extract_image;
 
+use std::fs::{remove_dir_all, create_dir};
+
 fn main() {
     fuzz!(|data: &[u8]| {
-        let _ = extract_image(data, &String::from("/tmp"));
+        let _ = remove_dir_all(String::from("/tmp/squashfuzz"));
+        let _ = create_dir(String::from("/tmp/squashfuzz"));
+        let _ = extract_image(data, &String::from("/tmp/squashfuzz"));
     })
 }
